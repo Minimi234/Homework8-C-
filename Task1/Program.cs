db1bef7e33913be7b76be81f80bf23695b86d9d0
Anisa <An1sa_jon@mail.ru>
2022-09-10 16:09:02 +0400

    Task54

---
+++ Task1/Program.cs
@@ -0,0 +1,70 @@
+﻿int Promt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GenerateArray(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+
+    }
+    Console.WriteLine();
+}
+
+int[,] SortRowNumbers(int[,] array)
+{
+    int temp = 0;
+    int indexmax = 0;
+    int m = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            indexmax = 0;
+            for (int k = 0; k < array.GetLength(1) - j; k++)
+            {
+                if (array[i, k] > array[i, indexmax])
+                {
+                    indexmax = k;
+                }
+                m = k;
+            }
+            temp = array[i, indexmax];
+            array[i, indexmax] = array[i, m];
+            array[i, m] = temp;
+        }
+    }
+    return array;
+}
+
+int rows = Promt("Введите количество строк: ");
+int columns = Promt("Введите количество столбцов: ");
+
+int[,] array1 = GenerateArray(rows, columns, 1, 100);
+PrintArray(array1);
+Console.WriteLine();
+
+int[,] array2 = SortRowNumbers(array1);
+PrintArray(array2);
+
